064234f6df6d6297eb8db64dc5204d104b34234f
maxrmax <ringmaximilian@protonmail.com>
2025-01-16 15:31:31 +0100

    fizzbuzz

--- level1/fizzbuzz/fizzbuzz.c
+++ level1/fizzbuzz/fizzbuzz.c
@@ -11,6 +11,32 @@
 // If the number is a multiple of 5, it prints 'buzz' instead.
 
 // If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+
+// Example:
+
+// $>./fizzbuzz
+// 1
+// 2
+// fizz
+// 4
+// buzz
+// fizz
+// 7
+// 8
+// fizz
+// buzz
+// 11
+// fizz
+// 13
+// 14
+// fizzbuzz
+// [...]
+// 97
+// 98
+// fizz
+// buzz
+// $>
+
 #include <unistd.h>
 
 void writenumber(int i)
@@ -39,28 +65,3 @@ int main(void)
 		write(1, "\n", 1);
 	}
 }
-
-// Example:
-
-// $>./fizzbuzz
-// 1
-// 2
-// fizz
-// 4
-// buzz
-// fizz
-// 7
-// 8
-// fizz
-// buzz
-// 11
-// fizz
-// 13
-// 14
-// fizzbuzz
-// [...]
-// 97
-// 98
-// fizz
-// buzz
-// $>
